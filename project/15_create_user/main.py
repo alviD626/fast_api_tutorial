05c73d4f27fb260024818c265f4e28dffdec9e20
Ali Mostakim Alvi <alvi@nitex.com>
2025-01-21 17:58:19 +0600

    add new  topics

--- project/15_create_user/main.py
+++ project/15_create_user/main.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 import schemas, models
 from database import engine, SessionLocal
@@ -46,15 +47,24 @@ def update(id,request:schemas.Blog,db:Session=Depends(get_db)):
     db.commit()
     return "updated"
 
-@app.get("/blog")
+@app.get("/blog",response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get("/blog/{id}",status_code=200)
+@app.get("/blog/{id}",status_code=200,response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Blog with the id {id} is not available")
     return blog
+
+
+@app.post('/user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name = request.name, email = request.email, password = request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
